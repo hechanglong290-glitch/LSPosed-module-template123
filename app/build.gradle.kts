cc04094ea102af331eafcee73ef5027b3fe799b2
hechanglong290-glitch <hechanglong290@gmail.com>
2026-09-01 09:31:13 +0800

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,7 +8,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.module"
-        minSdk = 31
+        minSdk = 28
         versionCode = 1
         versionName = "1.0.0"
     }
